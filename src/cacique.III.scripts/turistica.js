55c0f93704b927f300f2f9745f16a54e8e627488
tony <lonson574@gmail.com>
2024-07-13 15:44:45 -0400

    wip cacique III turistica

---
+++ src/cacique.III.scripts/turistica.js
@@ -0,0 +1,39 @@
+const commonSeat = "p-1 m-1 w-2 flex-shrink flex-grow shadow-xl bg-blue-800 rounded-md";
+const bottomSeat = "pr-5 m-1 w-2 flex-shrink flex-grow shadow-xl bg-blue-800 rounded-md";
+const fullcontainerSizeSeat = "p-1 m-1 w-5/6 flex-shrink flex-grow shadow-xl bg-blue-800 rounded-md";
+
+function generateSeatsTuple(start, end, interval, id, seatStyle) {
+  const seats = document.getElementById(id);
+  for (let index = start; index <= end; index += interval) {
+    const newItem = document.createElement("div");
+    let indexString = index.toString();
+    newItem.className = seatStyle;
+    while (indexString.length < 3) {
+      indexString = "0" + indexString;
+    }
+    newItem.textContent = indexString;
+    seats.appendChild(newItem); // añadir el div al elemento seleccionado
+  }
+}
+function reverseTuple(start, end, interval, id, seatStyle) {
+  const seats = document.getElementById(id);
+  for (let index = start; index >= end; index -= interval) {
+    const newItem = document.createElement("div");
+    let indexString = index.toString();
+    newItem.className = seatStyle;
+    while (indexString.length < 3) {
+      indexString = "0" + indexString;
+    }
+    newItem.textContent = indexString;
+    seats.appendChild(newItem); // añadir el div al elemento seleccionado
+  }
+}
+function reverseRow(start, end, interval, id, seatStyle, newContainerId) {
+  const selector = document.getElementById(id);
+  const tableContainer = document.createElement("div");
+  tableContainer.id = newContainerId;
+  tableContainer.className = tableContainerStyles;
+  //tableContainer.style.transform = 'rotate(45deg)'
+  selector.appendChild(tableContainer);
+  reverseTuple(start, end, interval, tableContainer.id, seatStyle);
+}
